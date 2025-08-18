c629d9c197a4833dd25ce92bb5e92a151c553c41
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2025-08-18 16:18:40 +0700

    init by learner

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ToDoList_Server"
+  }
+}
